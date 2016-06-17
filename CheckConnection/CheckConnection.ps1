ec080bf16621d251481e610206198a2bb787c318
rmatias_atos <rmatias_atos@TOC-A1ADAVW-061.rio2016.local>
2016-06-17 13:54:22 -0300

    Script to check the connection through telnet

---
+++ CheckConnection/CheckConnection.ps1
@@ -0,0 +1,22 @@
+﻿[xml]$xmlClients = Get-Content -Path C:\clients.xml
+$clients = $xmlClients['client-list'].ChildNodes
+foreach($client in $clients) {
+    $httpAddress = $client.http.Substring(7)
+    if ($httpAddress.Contains("/")) {
+        $httpAddress = $httpAddress.Substring(0, $httpAddress.IndexOf("/"))
+    }
+    if ($httpAddress.Contains(':')) {
+        $ip = $httpAddress.Substring(0, $httpAddress.IndexOf(":"))
+        $port = $httpAddress.Substring($httpAddress.IndexOf(":") + 1)
+    } else {
+        $ip = $httpAddress
+        $port = 80
+    }
+    try {
+        $connection = New-Object System.Net.Sockets.TcpClient -ArgumentList $ip,$port
+    } catch {
+		Write-Host $client.Name '>>' $ip':'$port '>>' $client.http
+    }
+}
+
+
